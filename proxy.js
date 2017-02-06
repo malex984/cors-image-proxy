c70912f26463ac0b7adb99d14994e7cce8f1594a
Oleksandr Motsak <http://goo.gl/mcpzY>
2017-02-06 22:30:13 +0100

    Quick and dirty fix to proxy http

--- proxy.js
+++ proxy.js
@@ -1,67 +1,30 @@
-var http = require('http'),
-    httpProxy = require('http-proxy');
-    url = require('url');
-    request = require('request');
+var http = require('http'), httpProxy = require('http-proxy'); url = require('url'); request = require('request');
 
-function processRequest(req, res, proxy, new_req){
-  req.url = new_req.path
-  req.headers.host = new_req.host
+function processRequest(req, res, proxy, new_req){ //  console.log(new_req);
+  req.url = new_req.path;
+  req.headers.host = new_req.host;
   res.oldWriteHead = res.writeHead;
   res.writeHead = function(statusCode, headers) {
-    res.setHeader('Access-Control-Allow-Origin', '*')
-    res.setHeader('Access-Control-Allow-Credentials', true)
-    res.oldWriteHead(statusCode, headers);
+    res.setHeader('Access-Control-Allow-Origin', '*'); res.setHeader('Access-Control-Allow-Credentials', true); res.oldWriteHead(statusCode, headers);
   }
   try{
-    proxy.proxyRequest(req, res, {
-      host: req.headers.host.split(":")[0],
-      port: parseInt(req.headers.host.split(":")[1]) || 80
-    });
+    proxy.proxyRequest(req, res, { host: req.headers.host.split(":")[0], port: parseInt(req.headers.host.split(":")[1]) || 80 });
   }catch(err){
     console.log(err);
   }
 }
 
 httpProxy.createServer(function (req, res, proxy) {
-  var url_path = req.url.slice(1)
-  if(url_path == "crossdomain.xml"){
-    proxy.proxyRequest(req, res, {
-      host: '127.0.0.1',
-      port: 9000
-    });
-  }
+  var url_path = req.url.slice(1);
+  if(url_path == "crossdomain.xml"){ proxy.proxyRequest(req, res, { host: '127.0.0.1', port: 9000 }); }
   else{
-    new_req = url.parse(url_path)
-    if(new_req.host == "avatars.io"){
-      request({
-        url: new_req.href,
-        followRedirect:false
-      }, function(error, response, body){
-        var final_req = url.parse(response.headers.location);
-        processRequest(req, res, proxy, final_req);
-      });
-    }
-    else{
-      if(new_req.host && new_req.host.match(/dgm59|cloudinary|twimg|instagram|facebook|fbcdn|distillery|qrserver|igcdn|naver/)){
-        processRequest(req, res, proxy, new_req);
-        console.log("allowed " + new_req.host)
-      }else{
-        console.log("disallowed " + new_req.host)
-        res.writeHead(400);
-        res.write('Not Allowed');
-        res.end();
-      }
-    }
+   new_req = url.parse("http://" + url_path); // NOTE: main limitation!!!
+   processRequest(req, res, proxy, new_req);
   }
 }).listen(8000);
 
-http.createServer(function (req, res) {
-  res.writeHead(200, { 'Content-Type': 'text/xml' });
-  res.write('<?xml version="1.0"?>\n<!DOCTYPE cross-domain-policy SYSTEM "http://www.macromedia.com/xml/dtds/cross-domain-policy.dtd">\n<cross-domain-policy>\n<allow-access-from domain="*" />\n</cross-domain-policy>\n');
-  res.end();
+http.createServer(function (req, res) { 
+  res.writeHead(200, { 'Content-Type': 'text/xml' }); res.write('<?xml version="1.0"?>\n<!DOCTYPE cross-domain-policy SYSTEM "http://www.macromedia.com/xml/dtds/cross-domain-policy.dtd">\n<cross-domain-policy>\n<allow-access-from domain="*" />\n</cross-domain-policy>\n');  res.end();
 }).listen(9000);
 
-
-process.on('uncaughtException', function(e){
-  console.log(e)
-});
+process.on('uncaughtException', function(e){  console.log(e); });
